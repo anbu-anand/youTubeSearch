bee22ce7a788fab578f4ad9578b98ccc9318f300
Anbarasan <37935894+anbuanand-github@users.noreply.github.com>
2020-12-17 23:08:39 +0530

    Initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ""
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+      term: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Search:</label>
+            <input
+              value={this.state.term}
+              type="text"
+              onChange={(e) => this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
